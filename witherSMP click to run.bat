257abc47977a6ad265da53f1d6b7139d2de41a64
gokul6350 <64578167+gokul6350@users.noreply.github.com>
2021-07-17 11:48:14 +0530

    Add files via upload

---
+++ witherSMP click to run.bat
@@ -0,0 +1,23 @@
+@echo off
+echo ==========================================
+echo             /\
+echo            /  \
+echo           /    \      /\
+echo          /      \    /  \
+echo         /        \  /    \
+echo        /          \/      \
+echo       /     Gun Top        \
+echo      /          SOFTWARE    \
+echo     /  GB GAMING YT  FoR     \
+echo    / BONFIRE NETWORK          \
+echo ============================================
+echo  Reaching the File
+echo  Reaching the File
+echo  Reaching the File
+echo Be Sure To Subscibe ME @ www.youtube.com/gbgamingyt
+echo press ENTER 
+pause
+copy mods C:\Users\admin\AppData\Roaming\.minecraft\mods
+pause
+pause
+
