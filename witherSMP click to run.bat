792f2093b0d4ed3f8888c3526ae0b1c221c2375c
gokul6350 <64578167+gokul6350@users.noreply.github.com>
2021-07-17 11:50:27 +0530

    Update witherSMP click to run.bat

--- witherSMP click to run.bat
+++ witherSMP click to run.bat
@@ -15,6 +15,7 @@ echo  Reaching the File
 echo  Reaching the File
 echo  Reaching the File
 echo Be Sure To Subscibe ME @ www.youtube.com/gbgamingyt
+echo Download from https://github.com/gokul6350/wither-SMP/archive/refs/heads/main.zip
 echo press ENTER 
 pause
 copy mods C:\Users\admin\AppData\Roaming\.minecraft\mods
